64f2698c40d04b2b598d74d85d3f438a4669bd0b
Alexander Oniani <alexanderoniani6@gmail.com>
2025-05-01 12:58:06 +0400

    Add column and table mappings to Player model for EF Core SQLite integration

--- data/Models/Tennis/Player.cs
+++ data/Models/Tennis/Player.cs
@@ -1,15 +1,35 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Data.Models.Tennis
 {
+    [Table("tennis_players")]
     public class Player
     {
+        [Column("player_id")]
         public int PlayerId { get; set; }
+
+        [Column("first_name")]
         public string FirstName { get; set; }
+
+        [Column("last_name")]
         public string LastName { get; set; }
+
+        [Column("hand")]
         public string Hand { get; set; }
+
+        [Column("date_of_birth")]
         public DateTime? Dob { get; set; }
+
+        [Column("ioc")]
         public string Ioc { get; set; }
+
+        [Column("country")]
         public string Country { get; set; }
+
+        [Column("height")]
         public int? Height { get; set; }
+
+        [Column("wikidata_id")]
         public string WikiDataId { get; set; }
     }
 }
